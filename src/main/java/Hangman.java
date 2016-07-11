b500318fedd9ada1acb4fb8c589bec906ee0754e
hetianguang <hetianguang123@163.com>
2016-07-11 20:26:57 +0800

    test travisCI and gradle

---
+++ src/main/java/Hangman.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+/**
+ * Created by Tiger on 2016/7/11.
+ */
+public class Hangman {
+    private final String word;
+    private int times;
+    private String guessed_chars_and_ture;
+    private String guessed_chars_but_false;
+    public static final String STATUS_1 = "you need to continue";
+    public static final String STATUS_2 = "you are successful";
+    public static final String STATUS_3 = "you have failed";
+
+    public Hangman(String str) {
+        this.word = str;
+        this.times = str.length()+1;
+        this.guessed_chars_and_ture = "aeiou";
+        this.guessed_chars_but_false = "";
+    }
+
+    public  int word_length() {
+        return word.length();
+    }
+
+    public  String show_word() {
+        return word;
+    }
+
+    public  String incompleter_word() {
+        String str = "";
+        char[] ch = word.toCharArray();
+        int i = 0;
+        while (i < word.length()) {
+
+            str += isBoolean_not_in_guessed_chars(ch[i]) ? "_" : String.valueOf(ch[i]);
+            i++;
+        }
+        return str;
+    }
+
+    private boolean isBoolean_not_in_guessed_chars(char ch) {
+        return (guessed_chars_and_ture.indexOf(ch) ==-1 && guessed_chars_but_false.indexOf(ch) ==-1);
+    }
+
+
+    public  int Times() {
+        return times;
+    }
+
+    public  String guessed_chars() {
+
+        return guessed_chars_and_ture;
+    }
+
+    public void guess(char ch) {
+        if(isBoolean_not_in_guessed_chars(ch))
+        {
+            --times;
+            if(isaBoolean_in_word(ch))
+                guessed_chars_and_ture += String.valueOf(ch);
+            else
+                guessed_chars_but_false += String.valueOf(ch);
+        }
+        else
+            System.out.println("you guessed!!!");
+
+    }
+
+    private boolean isaBoolean_in_word(char ch) {
+        return word.indexOf(ch)!=-1;
+    }
+
+    public  String show_current_word() {
+        return incompleter_word();
+    }
+
+    public  String Game_Status() {
+        boolean equals = isOK_guess();
+        return times > 0 ? equals ? STATUS_2 : STATUS_1 : equals ? STATUS_2 : STATUS_3;
+
+    }
+
+    private boolean isOK_guess() {
+        return word.equals(show_current_word());
+    }
+
+    public static void main(String[] args)
+    {
+        Hangman hangman = new Hangman("apple");
+        Scanner sc = new Scanner(System.in);
+        while(STATUS_1.equals(hangman.Game_Status()))
+        {
+            System.out.println( hangman.show_current_word());
+            System.out.println("please guess"+"\n");
+            hangman.guess(sc.next().charAt(0));
+            System.out.println("you have " + hangman.Times() +"times chances\n");
+            System.out.println("current the status of guess word" + hangman.show_current_word());
+            System.out.println( hangman.Game_Status());
+
+        }
+
+    }
+}
---
+++ src/test/java/HangmanTest.java
@@ -0,0 +1,82 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tiger on 2016/7/11.
+ */
+public class HangmanTest {
+    @Test
+    public void should_get_attribute_when_the_word_is_apple(){
+        Hangman hangman = new Hangman("apple");
+        assertThat(hangman.word_length(),is(5));
+        assertThat(hangman.show_word(),is("apple"));
+        assertThat(hangman.incompleter_word(),is("a___e"));
+        assertThat(hangman.Times(),is(6));
+    }
+    @Test
+    public void should_get_attribute_when_the_word_is_orange(){
+        Hangman hangman = new Hangman("orange");
+        assertThat(hangman.word_length(),is(6));
+        assertThat(hangman.show_word(),is("orange"));
+        assertThat(hangman.incompleter_word(),is("o_a__e"));
+        assertThat(hangman.Times(),is(7));
+    }
+    @Test
+    public void shuold_get_guessWord_status_when_guess_the_word_is_p(){
+        Hangman hangman = new Hangman("apple");
+        hangman.guess('p');
+        assertThat(hangman.guessed_chars(),is("aeioup"));
+        assertThat(hangman.Times(),is(5));
+        assertThat(hangman.show_current_word(),is("app_e"));
+    }
+    @Test
+    public void shuold_get_guessWord_status_when_guess_the_word_is_e(){
+        Hangman hangman = new Hangman("apple");
+        hangman.guess('p');
+        hangman.guess('e');
+        assertThat(hangman.guessed_chars(),is("aeioup"));
+        assertThat(hangman.Times(),is(5));
+        assertThat(hangman.show_current_word(),is("app_e"));
+        assertThat(hangman.Game_Status(),is("you need to continue"));
+        System.out.println("4444");
+    }
+    @Test
+    public void shuold_get_guessWord_status_when_guess_the_word_is_l(){
+        Hangman hangman = new Hangman("apple");
+        hangman.guess('l');
+        assertThat(hangman.guessed_chars(),is("aeioul"));
+        assertThat(hangman.Times(),is(5));
+        assertThat(hangman.show_current_word(),is("a__le"));
+        assertThat(hangman.Game_Status(),is("you need to continue"));
+        System.out.println("3333");
+    }
+
+    @Test
+    public void shuold_get_successful_status_when_guess_the_word_is_p_and_l(){
+        Hangman hangman = new Hangman("apple");
+        hangman.guess('p');
+        hangman.guess('l');
+        assertThat(hangman.guessed_chars(),is("aeioupl"));
+        assertThat(hangman.Times(),is(4));
+        assertThat(hangman.show_current_word(),is("apple"));
+        assertThat(hangman.Game_Status(),is("you are successful"));
+        System.out.println("22222");
+    }
+    @Test
+    public void shuold_get_failed_status(){
+        Hangman hangman = new Hangman("apple");
+        hangman.guess('p');
+        hangman.guess('f');
+        hangman.guess('k');
+        hangman.guess('m');
+        hangman.guess('c');
+        hangman.guess('s');
+        assertThat(hangman.guessed_chars(),is("aeioup"));
+        assertThat(hangman.Times(),is(0));
+        assertThat(hangman.show_current_word(),is("app_e"));
+        assertThat(hangman.Game_Status(),is("you have failed"));
+        System.out.println("11111");
+    }
+}
